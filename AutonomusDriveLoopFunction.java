332438ae938250a5fdf506856c4c793355ef5480
SebiCoroian <escapemcro@gmail.com>
2017-01-10 23:58:54 +0200

    Add files via upload

---
+++ AutonomusDriveLoopFunction.java
@@ -0,0 +1,48 @@
+public void loop()
+
+{
+//Is an IR signed detected?
+if (irSeeker.signalDetected())
+
+{
+
+// an IR signal is detected
+double angle = irSeeker.getAngle();
+double strength = irSeeter.getAngle();
+
+//which direction should we move?
+if (angle < 0) 
+{
+
+//we need to move to the left
+motorRight.setPower(MOTOR_POWER); 
+motorLeft.setPower(-MOTOR_POWER); 
+
+}
+ 
+else if (angle > 0)
+{
+//we need to move to the right
+motorRight.setPower(-MOTOR_POWER);
+motorLeft.setPower(MOTOR_POWER);
+}
+else if (strength <HOLD_IR_SIGNAL_STRENGTH)
+{
+// the IR signal is weak, approach
+motorRight.setPower(MOTOR_POWER);
+motorLeft.setPower(MOTUR POWER);
+} 
+else 
+{
+// the IR signal Is strong, stay here
+motorRight.set0ower(0.0);
+motorLeft.setPower(0.0);
+}
+ 
+}
+else
+{
+//no IR signal is detected
+motorRight.setPower(0.0);
+motorLeft.setPower(0.0);
+}
